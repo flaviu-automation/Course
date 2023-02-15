a66ef6a0acc894ab5aa4513430ac7e0bb2990236
flaviu-automation <flaviu.marciut@outlook.com>
2023-02-15 19:26:33 +0200

    1st commit

---
+++ src/main/java/taudemo/WithAssertionsTest.java
@@ -0,0 +1,16 @@
+package taudemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WithAssertionsTest {
+    @Test
+    public void firstAssertion() {
+        assertEquals(2, 2);
+    }
+    @Test
+    public void secondAssertion(){
+        assertEquals(45,45);
+    }
+}
